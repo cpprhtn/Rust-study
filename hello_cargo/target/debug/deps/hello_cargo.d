653209b7f34a7cb9469cce6b941e811aaa8e24e3
Lee_JunWon <cpprhtn@naver.com>
2020-12-06 18:00:56 +0900

    12/6

---
+++ hello_cargo/target/debug/deps/hello_cargo.d
@@ -0,0 +1,5 @@
+/Users/cpprhtn/Desktop/git_local/Rust-study/hello_cargo/target/debug/deps/hello_cargo: src/main.rs
+
+/Users/cpprhtn/Desktop/git_local/Rust-study/hello_cargo/target/debug/deps/hello_cargo.d: src/main.rs
+
+src/main.rs:
---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/Users/cpprhtn/Desktop/git_local/Rust-study/hello_cargo/target/debug/hello_cargo: /Users/cpprhtn/Desktop/git_local/Rust-study/hello_cargo/src/main.rs
