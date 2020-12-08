9df13bcf4199a27778a01fa013cb89b6c19b778a
Lee_JunWon <cpprhtn@naver.com>
2020-12-08 21:38:45 +0900

    12/8

--- slices/src/main.rs
+++ slices/src/main.rs
@@ -10,8 +10,20 @@ fn first_word(s: &String) -> usize {
         }
     }
 
-    //공백이 아닐 경우에는 문자열 길이 반환
+    // 공백이 아닐 경우에는 문자열 길이 반환
     s.len()
+}
+
+fn first_word_str(s: &str) -> &str {
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &s[0..i];
+        }
+    }
+
+    &s[..]
 }
 
 fn main() {
@@ -21,9 +33,50 @@ fn main() {
 
     s.clear(); // String 내용을 ""(빈내용)으로 변경
 
-    //스트링 슬라이스
+    // 스트링 슬라이스
     let s = String::from("hello world");
 
     let hello = &s[0..5];
     let world = &s[6..11];
+
+    //
+    let s = String::from("hello");
+    // 인덱스 0부터 시작시에는 범위에 0 생략가능
+    let slice = &s[0..2];
+    let slice = &s[..2];
+    // 인덱스 마지막까지 범위시에는 끝 숫자 생략가능
+    let len = s.len();
+    let slice = &s[3..len];
+    let slice = &s[3..];
+    // 전범위
+    let slice = &s[..];
+
+    //
+    let word2 = first_word_str(&s); // word = 5
+
+    // s의 타입: &str
+    let s = "Hello, world!";
+
+    //&String과 &str은 같은의미
+
+    //
+    let my_string = String::from("hello world");
+
+    // first_word가 `String`의 슬라이스로 동작
+    let word3 = first_word_str(&my_string[..]);
+
+    let my_string_literal = "hello world";
+
+    // first_word가 스트링 리터럴의 슬라이스로 동작
+    let word4 = first_word_str(&my_string_literal[..]);
+
+    // 스트링 리터럴 또한 스트링 슬라이스에 포함
+    let word5 = first_word_str(my_string_literal);
+
+    println!("1: {}\n2: {}\n3: {}\n4: {}\n5: {}",word, word2,word3, word4, word5);
+
+    //
+    let a = [1, 2, 3, 4, 5];
+    //이 슬라이스는 &[i32] 타입을 가짐
+    let slice = &a[1..3];
 }
