9b65a4bc487ba26659a425916c1569650bb75f6f
Lee_JunWon <cpprhtn@naver.com>
2020-12-11 17:09:29 +0900

    12/11

---
+++ match-control-flow/src/main.rs
@@ -0,0 +1,56 @@
+
+fn main() {
+    #[derive(Debug)] // So we can inspect the state in a minute
+    enum UsState {
+        Alabama,
+        Alaska,
+        // ... etc
+    }
+
+    enum Coin {
+        Penny,
+        Nickel,
+        Dime,
+        Quarter(UsState),
+    }
+
+    //ver1
+    /*
+    fn value_in_cents(coin: Coin) -> u32 {
+        match coin {
+            Coin::Penny => 1,
+            Coin::Nickel => 5,
+            Coin::Dime => 10,
+            Coin::Quarter => 25,
+        }
+    }
+    */
+
+    //ver2
+    /*
+    fn value_in_cents(coin: Coin) -> u32 {
+        match coin {
+            Coin::Penny => {
+                println!("Lucky penny!");
+                1
+            },
+            Coin::Nickel => 5,
+            Coin::Dime => 10,
+            Coin::Quarter => 25,
+        }
+    }
+    */
+
+    //ver3
+    fn value_in_cents(coin: Coin) -> u32 {
+        match coin {
+            Coin::Penny => 1,
+            Coin::Nickel => 5,
+            Coin::Dime => 10,
+            Coin::Quarter(state) => {
+                println!("State quarter from {:?}!", state);
+                25
+            },
+        }
+    }
+}
---
+++ match-control-flow/target/debug/match-control-flow.d
@@ -0,0 +1,1 @@
+/Users/cpprhtn/Desktop/git_local/Rust-study/match-control-flow/target/debug/match-control-flow: /Users/cpprhtn/Desktop/git_local/Rust-study/match-control-flow/src/main.rs
