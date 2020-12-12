8033831490563635f497f644312dc374f5b6976d
Lee_JunWon <cpprhtn@naver.com>
2020-12-12 22:19:50 +0900

    12/12

--- if-let-control-flow/src/main.rs
+++ if-let-control-flow/src/main.rs
@@ -1,3 +1,47 @@
+
+
 fn main() {
-    println!("Hello, world!");
+    // ver1 - match
+    let some_u8_value = Some(3u8);
+    match some_u8_value {
+        Some(3) => println!("three"),
+        _ => (),
+    }
+
+    // ver1 - if let
+    if let Some(3) = some_u8_value {
+        println!("three");
+    }
+
+    
+    #[derive(Debug)]
+    enum UsState {
+        Alabama,
+        Alaska,
+    }
+
+    enum Coin {
+        Penny,
+        Nickel,
+        Dime,
+        Quarter(UsState),
+    }
+    let coin = Coin::Penny;
+    let mut count = 0;
+    // ver2 - match
+    /*
+    let mut count = 0;
+    match coin {
+        Coin::Quarter(state) => println!("State quarter from {:?}!", state),
+        _ => count += 1,
+    }
+    */
+
+    // ver2 - if let
+    if let Coin::Quarter(state) = coin {
+        println!("State quarter from {:?}!", state);
+    } 
+    else {
+        count += 1;
+    }
 }
---
+++ if-let-control-flow/target/debug/if-let-control-flow.d
@@ -0,0 +1,1 @@
+/Users/cpprhtn/Desktop/git_local/Rust-study/if-let-control-flow/target/debug/if-let-control-flow: /Users/cpprhtn/Desktop/git_local/Rust-study/if-let-control-flow/src/main.rs
