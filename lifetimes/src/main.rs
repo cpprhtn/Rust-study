b4d1962a9dcb57a8d236387a83403f6a0ab39c01
Lee_JunWon <cpprhtn@naver.com>
2020-12-23 21:44:36 +0900

    12/23

--- lifetimes/src/main.rs
+++ lifetimes/src/main.rs
@@ -1,16 +1,28 @@
 
 
-// 라이프타임은 댕글링 참조자를 방지
+/*라이프타임은 댕글링 참조자를 방지*/
 
 
 // 함수에서의 제네릭 라이프타임
-// 두 스트링 슬라이스 중 긴 쪽을 찾기 위해 longest 함수를 호출하는 main 함수
+
+
 fn main() {
+
+    // 시그니처 내의 모든 참조자들이 동일한 라이프타임 'a를 가지고 있어야 함을 특정한 longest 함수 정의
     fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
         if x.len() > y.len() {
             x
-        } else {
+        } 
+        else {
             y
         }
+    }
+
+    // 서로 다른 구체적인 라이프타임을 가진 String 값의 참조자들을 이용한 longest 함수의 사용
+    let string1 = String::from("long string is long");
+    {
+        let string2 = String::from("xyz");
+        let result = longest(string1.as_str(), string2.as_str());
+        println!("The longest string is {}", result);
     }
 }
