12de237645253a2cc1e9cb169236d29611d6f5ea
Lee_JunWon <cpprhtn@naver.com>
2020-12-24 16:17:19 +0900

    12/24

--- lifetimes/src/main.rs
+++ lifetimes/src/main.rs
@@ -25,4 +25,15 @@ fn main() {
         let result = longest(string1.as_str(), string2.as_str());
         println!("The longest string is {}", result);
     }
+
+    // 
+    let novel = String::from("Call me Ishmael. Some years ago...");
+    let first_sentence = novel.split('.')
+        .next()
+        .expect("Could not find a '.'");
+    let i = ImportantExcerpt { part: first_sentence };
+}
+
+struct ImportantExcerpt<'a> {
+    part: &'a str,
 }
