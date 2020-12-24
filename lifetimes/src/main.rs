bab0da66c0f482de8ae00d5aec17fac64bdba63e
Lee_JunWon <cpprhtn@naver.com>
2020-12-24 20:04:07 +0900

    12/24

--- lifetimes/src/main.rs
+++ lifetimes/src/main.rs
@@ -45,8 +45,18 @@ fn main() {
     
         &s[..]
     }
+
+    // 정적 라이프타임 (Static lifetimes)
+    let s: &'static str = "I have a static lifetime.";
 }
 
 struct ImportantExcerpt<'a> {
     part: &'a str,
+}
+
+impl<'a> ImportantExcerpt<'a> {
+    fn announce_and_return_part(&self, announcement: &str) -> &str {
+        println!("Attention please: {}", announcement);
+        self.part
+    }
 }
