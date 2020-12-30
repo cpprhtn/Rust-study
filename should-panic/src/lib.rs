07adeedb23c8e3ca86ade37e5bbd72eebd82d8c1
Lee_JunWon <cpprhtn@naver.com>
2020-12-30 17:25:54 +0900

    12/30

--- should-panic/src/lib.rs
+++ should-panic/src/lib.rs
@@ -5,7 +5,10 @@ pub struct Guess {
     value: i32,
 }
 
+// 어떤 조건이 panic!을 일으키는지에 대한 테스트
+/*
 impl Guess {
+    
     pub fn new(value: i32) -> Guess {
         if value < 1 || value > 100 {
             panic!("Guess value must be between 1 and 100, got {}.", value);
@@ -25,5 +28,37 @@ mod tests {
         Guess::new(200);
     }
 }
+*/
+
+// 어떤 조건이 특정 패닉 메세지를 가진 panic!을 일으키는 테스트
+impl Guess {
+    pub fn new(value: i32) -> Guess {
+        if value < 1 {
+            panic!(
+                "Guess value must be greater than or equal to 1, got {}.",
+                value
+            );
+        } else if value > 100 {
+            panic!(
+                "Guess value must be less than or equal to 100, got {}.",
+                value
+            );
+        }
+
+        Guess { value }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    #[should_panic(expected = "Guess value must be less than or equal to 100")]
+    fn greater_than_100() {
+        Guess::new(200);
+    }
+}
+
 
 fn main() {}
