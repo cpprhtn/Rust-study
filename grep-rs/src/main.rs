d090764cab905971eb0cac890d1ebcb77a2a86fc
Lee_JunWon <cpprhtn@naver.com>
2020-12-31 15:12:32 +0900

    12/31

--- grep-rs/src/main.rs
+++ grep-rs/src/main.rs
@@ -3,4 +3,11 @@ use std::env;
 fn main() {
     let args: Vec<String> = env::args().collect();
     println!("{:?}", args);
+
+    // 쿼리와 파일이름 인자를 보관하는 두 변수 생성
+    let query = &args[1];
+    let filename = &args[2];
+
+    println!("Searching for {}", query);
+    println!("In file {}", filename);
 }
