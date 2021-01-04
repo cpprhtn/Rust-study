2623497c5caf456f272adac20b72539a87ae4448
Lee_JunWon <cpprhtn@naver.com>
2021-01-04 21:41:38 +0900

    1/4

--- grep-rs/src/main.rs
+++ grep-rs/src/main.rs
@@ -1,10 +1,14 @@
 use std::env;
 use std::fs;
+use std::process;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let config = Config::new(&args);
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
 
     println!("Searching for {}", config.query);
     println!("In file {}", config.filename);
