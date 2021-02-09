d5d0128b424e879cbd5a35a24dd7858cd7d8eb49
Lee_JunWon <cpprhtn@naver.com>
2021-02-09 23:04:14 +0900

    클로저 공부

--- closures/src/main.rs
+++ closures/src/main.rs
@@ -1,6 +1,8 @@
 use std::thread;
 use std::time::Duration;
 
+
+// 실행 시간이 2초 걸리는 가상의 계산을 대신하는 함수
 fn simulated_expensive_calculation(intensity: u32) -> u32 {
     println!("calculating slowly...");
     thread::sleep(Duration::from_secs(2));
