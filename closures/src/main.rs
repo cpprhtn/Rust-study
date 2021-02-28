af0ca67ca2d7067d12eba981fa03e13a234ca0b5
Lee_JunWon <cpprhtn@naver.com>
2021-03-01 00:45:57 +0900

    클로저 공부

--- closures/src/main.rs
+++ closures/src/main.rs
@@ -9,4 +9,15 @@ fn simulated_expensive_calculation(intensity: u32) -> u32 {
     intensity
 }
 
-fn main() {}
+fn generate_workout(intensity: u32, random_number: u32) {}
+
+// 사용자 입력과 임의의 숫자 생성을 시뮬레이션 하기 위한 main 함수와 하드코딩된 값
+fn main() {
+    let simulated_user_specified_value = 10;
+    let simulated_random_number = 7;
+
+    generate_workout(
+        simulated_user_specified_value,
+        simulated_random_number
+    );
+}
