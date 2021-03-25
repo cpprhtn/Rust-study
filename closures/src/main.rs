d73be144ea3e1513b98dfa4a5b1f913892e04143
Lee_JunWon <cpprhtn@naver.com>
2021-03-26 01:06:09 +0900

    simulated_expensive_calculation 에 대한 호출들을 한 곳으로 추출하고 결과를 expensive_result 변수에 저장하기

--- closures/src/main.rs
+++ closures/src/main.rs
@@ -9,7 +9,30 @@ fn simulated_expensive_calculation(intensity: u32) -> u32 {
     intensity
 }
 
-fn generate_workout(intensity: u32, random_number: u32) {}
+fn generate_workout(intensity: u32, random_number: u32) {
+    let expensive_result =
+        simulated_expensive_calculation(intensity);
+
+    if intensity < 25 {
+        println!(
+            "Today, do {} pushups!",
+            expensive_result
+        );
+        println!(
+            "Next, do {} situps!",
+            expensive_result
+        );
+    } else {
+        if random_number == 3 {
+            println!("Take a break today! Remember to stay hydrated!");
+        } else {
+            println!(
+                "Today, run for {} minutes!",
+                expensive_result
+            );
+        }
+    }
+}
 
 // 사용자 입력과 임의의 숫자 생성을 시뮬레이션 하기 위한 main 함수와 하드코딩된 값
 fn main() {
